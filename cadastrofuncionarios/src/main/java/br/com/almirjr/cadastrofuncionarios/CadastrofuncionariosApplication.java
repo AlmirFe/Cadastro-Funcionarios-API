6e939b76cab58fac60e255b4c2b3f8bd6668dbf1
Almir Ferreira <almirferreira.sj@gmail.com>
2021-12-28 11:15:23 -0300

    Adicionando configuração da database

---
+++ cadastrofuncionarios/src/main/java/br/com/almirjr/cadastrofuncionarios/CadastrofuncionariosApplication.java
@@ -0,0 +1,13 @@
+package br.com.almirjr.cadastrofuncionarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrofuncionariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrofuncionariosApplication.class, args);
+	}
+
+}
---
+++ cadastrofuncionarios/src/test/java/br/com/almirjr/cadastrofuncionarios/CadastrofuncionariosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.almirjr.cadastrofuncionarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrofuncionariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
