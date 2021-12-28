54d6e8084a22cf2228464f1a8a9b1ec384bfeebf
Almir Ferreira <almirferreira.sj@gmail.com>
2021-12-28 11:44:07 -0300

    Adicionando modelo funcionario

---
+++ cadastrofuncionarios/src/main/java/br/com/almirjr/cadastrofuncionarios/model/Funcionarios.java
@@ -0,0 +1,56 @@
+package br.com.almirjr.cadastrofuncionarios.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="funcionarios_tb")
+public class Funcionarios implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	private long id; 
+	private String nome; 
+	private String setor; 
+	private String dataNascimento;
+	private String cpf;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSetor() {
+		return setor;
+	}
+	public void setSetor(String setor) {
+		this.setor = setor;
+	}
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	} 
+	
+	
+
+}
